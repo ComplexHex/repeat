95a675f202cbd303baeda069e15c67b14892f398
ComplexHex <60725024+ComplexHex@users.noreply.github.com>
2022-04-14 20:27:02 +0300

    v8.001

---
+++ lesson8/MainApp.java
@@ -0,0 +1,21 @@
+package lesson8;
+
+public class MainApp {
+    public static void main(String[] args) {
+        doSomething(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Java");
+            }
+        });
+
+        doSomething(() -> {
+            System.out.println(100);
+        });
+
+
+    }
+
+   public static void doSomething(Runnable runnableObj){
+       runnableObj.run();
+   }
+}
