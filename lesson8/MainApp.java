f0ce2300f2c9338aee84907284d6c8f17c884ec6
ComplexHex <60725024+ComplexHex@users.noreply.github.com>
2022-04-16 12:39:48 +0300

    v8.3

--- lesson8/MainApp.java
+++ lesson8/MainApp.java
@@ -1,7 +1,13 @@
 package lesson8;
 
+import java.util.concurrent.Callable;
+
 public class MainApp {
+
+
     public static void main(String[] args) {
+
+        MyNumber myNumber;
         doSomething(() -> {
             for (int i = 0; i < 5; i++) {
                 System.out.println("Java");
@@ -14,12 +20,20 @@ public class MainApp {
 
         doSomething(() -> {
             int n = 5;
-            System.out.println(n*1);
+            System.out.println(n * 1);
         });
+        myNumber = () -> {
+            return 123.45;
+        };
+
+        myNumber = () -> Math.random()*100;
 
     }
 
-   public static void doSomething(Runnable runnableObj){
-       runnableObj.run();
-   }
+
+    public static void doSomething(Runnable runnableObj) {
+        runnableObj.run();
+    }
+
+
 }
