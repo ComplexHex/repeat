adccc8fafdf54fb4c796881377be78d6feb62214
ComplexHex <60725024+ComplexHex@users.noreply.github.com>
2022-05-02 19:28:33 +0300

    v1.3

--- game/controller/PlayersController.java
+++ game/controller/PlayersController.java
@@ -1,17 +1,29 @@
 package com.game.controller;
 
 import com.game.entity.Player;
+import com.game.repository.PlayerRepository;
 import com.game.service.PlayerService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @Controller
-@RequestMapping("rest/players")
-
+@RequestMapping("/rest/players")
 public class PlayersController {
     private PlayerService playerService;
 
@@ -20,9 +32,10 @@ public class PlayersController {
         this.playerService = playerService;
     }
 
-    @GetMapping("")
-    public List<Player> list(){
-        return playerService.listAllPlayer();
-    }
+    @GetMapping
+    public String showPlayers(Model model){
+        model.addAttribute("players", playerService.listAllPlayer());
+        return "players";
 
+    }
 }
