3afd3a6f6a2a4e5f486a885c56a60ff6c4320f0f
ComplexHex <60725024+ComplexHex@users.noreply.github.com>
2022-06-23 20:38:13 +0300

    Add files via upload

---
+++ regex/Regex1.java
@@ -0,0 +1,12 @@
+package regex;
+
+public class Regex1 {
+    public static void main(String[] args) {
+        String s = "Lana Sergeevna, Russia, Sevastopol, Lenin street, 51, flat 48,"
+                + "email: lanas@gmail.com, Postcode: 98311, Phone Number: +79788977776"
+                + "Lena ivanova, Russia, Kerch, Kim street, 43, flat 12,"
+                + "email: lenusik@gmail.ru, Postcode: 478896, Phone Number: +79785664849";
+
+
+    }
+}
